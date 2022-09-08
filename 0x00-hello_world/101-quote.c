e83e0a8918b536dce6633ac98254b4bdded8dc7a
Amkeez252 <auwaamalik@gmail.com>
2022-09-08 16:29:20 -0700

    my corection advance task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include <unistd.h>
 /**
- *  * main - Entry point
- *  * Return: Always 1 (Success)
+ *  *main-program entry point.
+ *  *Return: 1 on success.
 */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful
-	\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
 }
