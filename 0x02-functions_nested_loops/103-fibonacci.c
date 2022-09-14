b7352922c6c82c1b4d902cb301d20643cfc0c44b
Amkeez252 <auwaamalik@gmail.com>
2022-09-14 16:25:07 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,9 @@
+(21 sloc)  389 Bytes
 #include <stdio.h>
-/*
+
+/**
  * main - Entry point
- * Return: Always 0 (Success)*
+ * Return: Always 0 (Success)
 */
 
 int main(void)
@@ -11,7 +13,6 @@ int main(void)
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
-
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
@@ -19,6 +20,7 @@ int main(void)
 			sum += fibonacci[i];
 	}
 	printf("%ld\n", sum);
+
 	return (0);
 }
 
