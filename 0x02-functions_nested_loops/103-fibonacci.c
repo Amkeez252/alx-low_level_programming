78774788a3b67ce9a1aafcc8dcf1aee5ea3c03af
Amkeez252 <auwaamalik@gmail.com>
2022-09-14 16:27:51 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,6 +13,7 @@ int main(void)
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
+
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
@@ -20,7 +21,6 @@ int main(void)
 			sum += fibonacci[i];
 	}
 	printf("%ld\n", sum);
-
 	return (0);
 }
 
