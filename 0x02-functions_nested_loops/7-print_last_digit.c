4949c588c44f45cf894034dbde43955305a25695
Amkeez252 <auwaamalik@gmail.com>
2022-09-13 20:57:35 -0700

    7 abd 1 advanced task

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,10 +12,10 @@ int print_last_digit(int n)
 	int last_digit = n % 10;
 
 	if (n < 0)
-		n = -1 * (n % 10);
+		n = -1 * (last_digit);
 	else
-		n = n % 10;
-	_putchar((n % 10) + '0');
+		n = last_digit;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
 
