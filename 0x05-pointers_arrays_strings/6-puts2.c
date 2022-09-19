e80fc14ffbd9a727eb1794565def1e4498946966
Amkeez252 <auwaamalik@gmail.com>
2022-09-19 11:46:13 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: An input string
