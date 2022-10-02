5f3620e6990afcd4eddc37759a340ed83bf28c39
Amkeez252 <auwalamalik@gmail.com>
2022-10-01 23:43:42 -0700

    0x09-static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
-sasSaa _abs(int n);
+int _abs(int n);
 int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
@@ -20,5 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
 
+#endif
