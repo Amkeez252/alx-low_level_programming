dc9472a3f6c70f745472090b833d93860b096898
Amkeez252 <auwalamalik@gmail.com>
2022-10-02 22:38:22 -0700

    static_libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * File: main.h
+ * Auth: Amkeez252
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
