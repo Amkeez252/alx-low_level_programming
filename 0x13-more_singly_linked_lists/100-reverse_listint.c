d9b04ca2efe512e00d3048cc361706bdfa091963
Amkeez252 <auwalamalik@gmail.com>
2022-10-24 16:02:38 -0700

    Advanced projects

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - reverses a linked list
+ *@head: pointer to the head pointer of a linked list
+ *Return: pointer to the first node of the returned list
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current;
+	listint_t *nxt;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	current = *head;
+	*head = current->next;
+	nxt = (*head)->next;
+
+	current->next = NULL;
+
+	while (nxt != NULL)
+	{
+		(*head)->next = current;
+		current = *head;
+		*head = nxt;
+		nxt = nxt->next;
+	}
+	(*head)->next = current;
+	return (*head);
+}
+
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * free_listint_safe - function that frees a listint_t list
+ * @h: free
+ * Return: return listint
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	size_t c = 0;
+	listint_t *head, *temp;
+	long diff;
+
+	if (!h)
+		return (0);
+	head = *h;
+	*h = NULL;
+	while (head)
+	{
+		c++;
+		diff = head->next - head;
+		temp = head;
+		free(temp);
+		if (diff >= 0)
+			break;
+		head = head->next;
+	}
+	return (c);
+}
+
