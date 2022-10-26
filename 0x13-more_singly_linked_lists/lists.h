b94ca178a6a4e8206e5dc0c9c1656836d434d93b
Amkeez252 <auwalamalik@gmail.com>
2022-10-26 08:41:08 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -14,6 +14,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 size_t recursion_printer(const listint_t *h);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -31,4 +32,5 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 void free_list(listint_t *head);
 listint_t *find_listint_loop(listint_t *head);
+
 #endif /* _LISTS_H_ */
