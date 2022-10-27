30ae2a014abf7ac182db51d6568e4dc107d928f9
Amkeez252 <auwalamalik@gmail.com>
2022-10-27 16:48:24 -0700

    0x14-bit_manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,22 @@
 #include "main.h"
 
 /**
- * print_binary - A function that prints a binary number without % or /
- * @n: The number to be printed
- * Return: Nothing
+ * print_binary -  prints the binary representation of a number
+ * @n: integer to convert
  */
 
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
+	int i, flag;
 
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
-	while (max)
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
 	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
+		if ((n >> i) & 1)
 			flag = 1;
-		}
-		max >>= 1;
+		if (flag == 1)
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
 	}
 }
 
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - A function that sets a bit at given index to 1
- * @n: The number to set bit in
- * @index: The index to set bit at
- * Return: 1 if it worked, or -1 on error
+ * set_bit - sets bit at index to 1
+ * @n: pointer to number
+ * @index: index to change
+ * Return: 1 on success, -1 on failure
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int max = 0x01;
-
-	max <<= index;
-	if (max == 0)
+	if (index > (8 * sizeof(*n)) - 1)
 		return (-1);
-	*n |= max;
+	*n = *n | (1 << index);
 	return (1);
 }
 
